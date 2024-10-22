8b8edf28156fdad93f89865504acb0f88ea4d39e
danielta18 <153893558+danielta18@users.noreply.github.com>
2024-10-22 16:41:34 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function drawCard() {
+  // let button = document.getElementById("NewCard");
+  // button.addEventListener("click", function() {
+  // window.onload = function() {
+  let suitType = ["♥", "♠", "♦", "♣"];
+  let numberOption = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let suitPicker = Math.floor(Math.random() * suitType.length);
+  let numberPicker = Math.floor(Math.random() * numberOption.length);
+
+  let suit = suitType[suitPicker];
+  let number = numberOption[numberPicker];
+
+  if (suit == "♥" || suit == "♦") {
+    document.querySelector("#Lsuit").style.color = "red";
+    document.querySelector("#Rsuit").style.color = "red";
+  } else if (suit == "♠" || suit == "♣") {
+    document.querySelector("#Lsuit").style.color = "black";
+    document.querySelector("#Rsuit").style.color = "black";
+  }
+
+  document.querySelector("#Lsuit").innerHTML = suit;
+  document.querySelector("#Rsuit").innerHTML = suit;
+  document.querySelector("#number").innerHTML = number;
+  // };
+}
+
+setInterval(drawCard, 750);
